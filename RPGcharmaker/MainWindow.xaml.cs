08eb52a970e0647ae2b7e966e3c773356f8d906c
22003366ctc <22003366ctc@MA-CTC-INST-S08>
2023-03-13 11:57:06 -0400

    Add project files.

---
+++ RPGcharmaker/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPGcharmaker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RPGcharacter _character = new RPGcharacter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            updateStats();
+
+        }
+
+        private void buttonUpdateName_Click(object sender, RoutedEventArgs e)
+        {
+            _character.Name = nameInput.Text;
+        }
+
+        private void re_rolls_Click(object sender, RoutedEventArgs e)
+        {  
+            _character.Roll();
+            updateStats();
+        }
+
+        private void updateStats()
+        {
+            strengthText.Text = _character.Strength.ToString();
+            wisdomText.Text = _character.Wisdom.ToString();
+            dexterityText.Text = _character.Dexterity.ToString();
+            staminaText.Text = _character.Stamina.ToString();
+            intelligenceText.Text = _character.Intelligence.ToString();
+            charismaText.Text = _character.Charisma.ToString();
+        }
+    }
+}
