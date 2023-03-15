944123df345179788b66e29591014a069563de30
22003366ctc <22003366ctc@MA-CTC-INST-S08>
2023-03-15 14:10:58 -0400

    new

--- RPGcharmaker/MainWindow.xaml.cs
+++ RPGcharmaker/MainWindow.xaml.cs
@@ -20,12 +20,19 @@ namespace RPGcharmaker
     /// </summary>
     public partial class MainWindow : Window
     {
-        private RPGcharacter _character = new RPGcharacter();
+        private Random _rpg = new Random();
+        private RPGcharacter _character;
         public MainWindow()
         {
             InitializeComponent();
+            _character = new RPGcharacter(_rpg);
             updateStats();
-
+            string Junk = "";
+            for(int i = 0; i < 10; i++)
+            {
+                Junk += $"{RPGcharacter.RollDice(5, 20, _rpg).ToString()}\n";
+            }
+            MessageBox.Show(Junk);
         }
 
         private void buttonUpdateName_Click(object sender, RoutedEventArgs e)
@@ -34,9 +41,51 @@ namespace RPGcharmaker
         }
 
         private void re_rolls_Click(object sender, RoutedEventArgs e)
-        {  
+        {
             _character.Roll();
             updateStats();
+
+            double odds = .5;
+            _character.PartyMembers.Clear();
+
+            foreach (ListBoxItem i in listPotentialMembers.Items)
+            {
+                if (_rpg.NextDouble() > odds)
+                {
+                    RPGcharacter c = new RPGcharacter(_rpg)
+                    {
+                        Name = i.Content.ToString()
+                    };
+                    _character.PartyMembers.Add(c);
+                }
+                //junk
+                if(_character.PartyMembers.Count > 0)
+                {
+                     _character.PartyMembers[0].FavoriteColor = Brushes.BlanchedAlmond;
+                }
+               
+            }
+            Brush color1 = Brushes.Tomato;
+            Brush color2 = Brushes.Honeydew;
+            listPartyMembers.Items.Clear();
+            foreach(RPGcharacter c in _character.PartyMembers)
+            {
+                ListBoxItem i = new ListBoxItem();
+                if(c.FavoriteColor != null)
+                {
+                    i.Background= c.FavoriteColor;
+                }
+               else if (listPartyMembers.Items.Count % 2 == 0)
+                {
+                    i.Background = color1; 
+                }
+                else
+                {
+                    i.Background = color2;
+                }
+                i.Content = $"{c.Name} STR: {c.Strength} INT: {c.Intelligence} STA: {c.Stamina} DEX: {c.Dexterity} WIS: {c.Wisdom} CHA: {c.Charisma}";
+                listPartyMembers.Items.Add(i);
+            }
         }
 
         private void updateStats()
--- RPGcharmaker/RPGcharacter.cs
+++ RPGcharmaker/RPGcharacter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using System.Windows.Navigation;
 
 namespace RPGcharmaker
@@ -21,8 +22,10 @@ namespace RPGcharmaker
         private int _maxIntelligence = 20;
         private int _maxDexterity = 20;
         private int _maxStamina = 20;
-        private CharacterClasses _characterClass = CharacterClasses.None;
+        private Brush _favoriteColor;
+        private List<RPGcharacter> _partyMembers = new List<RPGcharacter>();
 
+        private CharacterClasses _characterClass = CharacterClasses.None;
         private Random _rng = new Random();
 
         #region Properties
@@ -30,20 +33,32 @@ namespace RPGcharmaker
         public long XP { get; set; }
         public int level { get; set; }
 
-        public CharacterClasses CharacterClass 
-        { get { return _characterClass; } 
-          set { _characterClass = value; } 
+        public CharacterClasses CharacterClass
+        { get { return _characterClass; }
+            set { _characterClass = value; }
         }
-        public int Strength {get { return _strength; }}
-        public int Charisma {get { return _charisma; }}
+        public int Strength { get { return _strength; } }
+        public int Charisma { get { return _charisma; } }
         public int Stamina { get { return _stamina; } }
-        public int Intelligence {get { return _intelligence; }}
+        public int Intelligence { get { return _intelligence; } }
         public int Dexterity { get { return _dexterity; } }
-        public int Wisdom {get { return _wisdom; }}
+        public int Wisdom { get { return _wisdom; } }
+        public Brush FavoriteColor
+        {
+            get { return _favoriteColor; }
+            set { _favoriteColor = value; }
+        }
+
+        public List<RPGcharacter> PartyMembers
+        {
+            get { return _partyMembers; }
+            set { _partyMembers = value; }
+        }
 
         #endregion Properties
-        public RPGcharacter()
+        public RPGcharacter(Random rng)
         {
+            _rng = rng;
             Roll();
         }
 
@@ -55,6 +70,17 @@ namespace RPGcharmaker
             _intelligence = _rng.Next(1, _maxIntelligence + 1);
             _stamina = _rng.Next(1, _maxStamina + 1);
             _dexterity = _rng.Next(1, _maxDexterity + 1);
+        }
+        public static int RollDice (int numberOfDice, int sidesPerdice, Random r)
+        {
+        int total = 0;
+
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                total = total + r.Next(1, sidesPerdice + 1);
+            }
+
+            return total;
         }
 
     }
